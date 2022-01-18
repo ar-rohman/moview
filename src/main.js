92033088b31abdaf3a4d62d7910d644939db3674
Rohman <rohmanpendo@gmail.com>
2022-01-18 14:17:03 +0700

    Quasar config

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue';
+import { quasar } from 'quasar';
 import App from './App.vue';
+import './assets/styles/index.sass';
 
-createApp(App).mount('#app');
+// createApp(App).mount('#app');
+const app = createApp(App);
+app.use(quasar, {});
+app.mount('#app');
