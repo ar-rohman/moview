b94653c9feae08b019384e00beda31fd3df5258b
Rohman <rohmanpendo@gmail.com>
2022-01-18 14:23:17 +0700

    move dev dependency to its location

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue';
-import { quasar } from 'quasar';
+import { Quasar } from 'quasar';
 import App from './App.vue';
 import './assets/styles/index.sass';
 
 // createApp(App).mount('#app');
 const app = createApp(App);
-app.use(quasar, {});
+app.use(Quasar, {});
 app.mount('#app');
