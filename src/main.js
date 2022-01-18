9f81356e02cfe971f764ae17aa685e2a914b6dfc
Rohman <rohmanpendo@gmail.com>
2022-01-18 14:28:40 +0700

    add vue-router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
+import router from './router';
 import { Quasar } from 'quasar';
 import App from './App.vue';
 import './assets/styles/index.sass';
 
 // createApp(App).mount('#app');
 const app = createApp(App);
+app.use(router);
 app.use(Quasar, {});
 app.mount('#app');
