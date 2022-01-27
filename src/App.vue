d52a1a13a268e2f1eeda2c8de91cfd972e22de01
Rohman <rohmanpendo@gmail.com>
2022-01-27 15:09:55 +0700

    add conditional to show or hide header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div
         class="font-sans text-gray-700 bg-white dark:bg-dark-900 dark:text-dark-200 min-h-screen max-w-screen-xl mx-auto">
-        <HeaderSection />
+        <HeaderSection v-if="route.meta.isShowHeader" />
         <main class="pb-20 sm:pb-4 pt-4 mx-4 sm:mx-10">
             <router-view />
         </main>
@@ -16,8 +16,7 @@ export default {
     components: { HeaderSection },
     setup() {
         const route = useRoute();
-        console.log(route);
-        return {};
+        return { route };
     },
 };
 </script>
