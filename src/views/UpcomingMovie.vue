654976fa4fb2c98121dd077964d28367be48283a
Rohman <rohmanpendo@gmail.com>
2022-01-31 12:24:59 +0700

    upcoming movie list

---
+++ src/views/UpcomingMovie.vue
@@ -0,0 +1,13 @@
+<template>
+    <InfinityScroll page-title="Upcoming Movies" service-url="movie/upcoming" />
+</template>
+
+<script>
+import InfinityScroll from '@/components/InfinityScroll.vue';
+
+export default {
+    components: {
+        InfinityScroll,
+    },
+};
+</script>
