48ecf868fbeb0d93fede109a9fb74ba90bfb3aa2
Rohman <rohmanpendo@gmail.com>
2022-02-03 09:31:01 +0700

    add show more component

---
+++ src/components/ShowMore.vue
@@ -0,0 +1,53 @@
+<template>
+    <div :class="textClass">
+        <!-- {{ content }} -->
+        <p class="inline" v-html="content"></p>
+        <span
+            class="text-red-500 font-semibold hover:text-red-600 cursor-pointer whitespace-nowrap select-none"
+            @click="toggleString">
+            {{ toggleMoreLess }}
+        </span>
+    </div>
+</template>
+
+<script>
+import { onMounted, ref, toRefs } from 'vue';
+import { turncateString } from '@/utils/stringManipulation';
+
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+        length: {
+            type: Number,
+            default: null,
+        },
+        textClass: {
+            type: String,
+            default: '',
+        },
+    },
+    setup(props) {
+        const isTruncated = ref(true);
+        const toggleMoreLess = ref(null);
+        const content = ref(null);
+        const { text, length } = toRefs(props);
+        const toggleString = () => {
+            if (isTruncated.value) {
+                content.value = turncateString(text.value, length.value);
+                isTruncated.value = false;
+                toggleMoreLess.value = ' show more';
+            } else {
+                content.value = turncateString(text.value);
+                isTruncated.value = true;
+                toggleMoreLess.value = ' show less';
+            }
+        };
+        onMounted(toggleString);
+
+        return { content, toggleMoreLess, toggleString };
+    },
+};
+</script>
