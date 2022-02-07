349c1a4008bc2dd30832a05312cd85d3ec7498e4
Rohman <rohmanpendo@gmail.com>
2022-02-07 14:22:33 +0700

    fix: fix contry code at free to watch page

--- src/views/movie/FreeMovie.vue
+++ src/views/movie/FreeMovie.vue
@@ -1,9 +1,14 @@
 <template>
-    <InfinityScroll page-title="Free Movies" service-url="discover/movie" :query="state.query" />
+    <InfinityScroll
+        v-if="state.query.watch_region"
+        page-title="Free Movies"
+        service-url="discover/movie"
+        :query="state.query" />
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
+import { useCountryCodeStore } from '@/stores/country';
 import InfinityScroll from '@/components/InfinityScroll.vue';
 
 export default {
@@ -12,12 +17,17 @@ export default {
     },
     provide: { detailLink: '/movie/detail' },
     setup() {
+        const countryCodeStore = useCountryCodeStore();
         const query = {
             include_adult: false,
-            watch_region: 'ID', // TODO
+            watch_region: '',
             with_watch_monetization_types: 'free',
         };
         const state = reactive({ query });
+        onMounted(async () => {
+            state.query.watch_region = await countryCodeStore.getCountryCode();
+        });
+
         return { state };
     },
 };
