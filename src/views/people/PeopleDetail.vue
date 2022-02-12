e0e37fafadbe3ce796a573a57d86be66d9635fb5
Rohman <rohmanpendo@gmail.com>
2022-02-12 09:49:09 +0700

    fix: people detail page

--- src/views/people/PeopleDetail.vue
+++ src/views/people/PeopleDetail.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="relative">
         <div class="absolute top-0">
-            <BackToPervious :text="personDetail.detail.data.name" />
+            <BackToPervious :text="detail.result.name" />
         </div>
     </div>
     <div class="flex flex-col lg:flex-row gap-10 lg:gap-20">
@@ -9,42 +9,42 @@
             <div class="flex flex-col xs:flex-row gap-x-4 w-ful">
                 <div class="min-w-[192px] -mx-4 xs:mx-0">
                     <img
-                        :src="personDetail.detail.data.image"
-                        :alt="personDetail.detail.data.name"
+                        :src="detail.result.image"
+                        :alt="detail.result.name"
                         class="xs:h-[256px] w-full rounded-none xs:rounded-2xl lg:rounded-3xl object-cover object-top" />
                 </div>
                 <div class="flex flex-col gap-y-4 lg:w-1/2 sm:pr-4 lg:pr-10">
                     <div class="my-6 xs:mb-6 xs:mt-0">
                         <div class="font-semibold sm:font-bold text-3xl">
-                            {{ personDetail.detail.data.name }}
+                            {{ detail.result.name }}
                         </div>
                         <div class="text-gray-500">
-                            {{ personDetail.detail.data.known_for_department }}
+                            {{ detail.result.known_for_department }}
                         </div>
                     </div>
                     <div>
                         <div class="font-semibold mb-1">Birthday</div>
-                        <p>{{ personDetail.detail.data.birthday || '-' }}</p>
+                        <p>{{ detail.result.birthday }}</p>
                     </div>
                     <div>
                         <div class="font-semibold mb-1">Deathday</div>
-                        <p>{{ personDetail.detail.data.deathday || '-' }}</p>
+                        <p>{{ detail.result.deathday }}</p>
                     </div>
                     <div>
                         <div class="font-semibold mb-1">Place of birth</div>
-                        <p>{{ personDetail.detail.data.place_of_birth || '-' }}</p>
+                        <p>{{ detail.result.place_of_birth }}</p>
                     </div>
                 </div>
             </div>
             <div class="flex flex-col md:flex-row gap-10">
-                <div v-if="personDetail.detail.data.biography" class="w-full md:w-2/3">
+                <div v-if="detail.result.biography" class="w-full md:w-2/3">
                     <div class="font-semibold mb-1">Biography</div>
-                    <ShowMore :text="personDetail.detail.data.biography" length="300" />
+                    <ShowMore :text="detail.result.biography" length="300" />
                 </div>
-                <div v-if="personDetail.detail.data.also_known_as" class="w-full md:w-1/3">
+                <div v-if="detail.result.also_known_as" class="w-full md:w-1/3">
                     <div class="font-semibold mb-1">Also known as</div>
                     <ul
-                        v-for="(item, index) in personDetail.detail.data.also_known_as"
+                        v-for="(item, index) in detail.result.also_known_as"
                         :key="index"
                         class="list-disc list-inside">
                         <li>{{ item }}</li>
@@ -53,7 +53,7 @@
             </div>
         </div>
         <div class="w-full lg:w-1/4 xs:mt-[60px]">
-            <PeopleList :data="personPopular.popular.data" title="Popular People" />
+            <PeopleList :data="popular" title="Popular People" />
         </div>
     </div>
 </template>
@@ -71,28 +71,18 @@ export default {
     components: { BackToPervious, ShowMore, PeopleList },
     setup() {
         const route = useRoute();
-        const detail = {
-            data: {},
-            isLoading: true,
-            isError: false,
-        };
-        const popular = {
-            data: {},
-            isLoading: true,
-            isError: false,
-        };
-        const personDetail = reactive({ detail });
-        const personPopular = reactive({ popular });
+        const detail = reactive({ result: {}, isLoading: true, isError: false });
+        const popular = reactive({ result: {}, isLoading: true, isError: false });
 
         const getPersonDetail = async () => {
             try {
                 const result = await PeopleService.getDetail(route.params.id);
                 const { data } = result;
-                personDetail.detail.data = peopleDetailResource(data);
-                personDetail.detail.isLoading = false;
-                personDetail.detail.isError = false;
+                detail.result = peopleDetailResource(data);
+                detail.isLoading = false;
+                detail.isError = false;
             } catch (error) {
-                personDetail.detail.isError = true;
+                detail.isError = true;
             }
         };
 
@@ -100,11 +90,11 @@ export default {
             try {
                 const result = await PeopleService.getPopular();
                 const { data } = result;
-                personPopular.popular.data = peopleListResource(data.results.slice(0, 5));
-                personPopular.popular.isLoading = false;
-                personPopular.popular.isError = false;
+                popular.result = peopleListResource(data.results.slice(0, 5));
+                popular.isLoading = false;
+                popular.isError = false;
             } catch (error) {
-                personPopular.popular.isError = true;
+                popular.isError = true;
             }
         };
 
@@ -119,7 +109,7 @@ export default {
         );
         onMounted(getPersonDetail);
         onMounted(getPopularPerson);
-        return { personDetail, personPopular };
+        return { detail, popular };
     },
 };
 </script>
