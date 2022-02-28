b6b92823b856ec5a4c8c1f80c3f046ca8c74fb88
Rohman <rohmanpendo@gmail.com>
2022-02-28 13:20:50 +0700

    test deploy

--- src/services/API.js
+++ src/services/API.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
-const accessToken =
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzRiZTlhNDViNDI3NDc2OTYwNzM1ODU0MDk2NmNiYiIsInN1YiI6IjYwOWI2OWJhOGQ3N2M0MDA3ODBlMWJmNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EJ6sCrSypjmXEXZ0EPU5u15_ynkWrYSA-z7XgZ9n_JQ';
+const accessToken = import.meta.env.VITE_API_KEY;
 
 export default {
     apiClient(url, param = null) {
